31d15b21492ba7964bcc9b5654de5a18820ec203
joelhagai <joel.hagai@gmail.com>
2018-02-26 00:08:58 +0100

    Adding some comments.

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,19 @@
+import {Readable} from "stream";
 import {checkInput, InputErrorEnum} from "./check-input";
 import {ConfigurationParser, orientationMapper} from "./configuration-parser-service";
 import {InstructionEnum} from "./instruction-enum";
 import {OrientationEnum} from "./orientation-enum";
 import {writeLine} from "./util/stdout-writer-util";
 
-const orientationEnumToLabel = new Map<OrientationEnum, string>();
-
-orientationMapper.forEach(
-    function (value, key) {
-        this.set(value, key);
-    }, orientationEnumToLabel);
-
+// Processing input to extract the file path.
 const args = process.argv.slice(2);
 const filePath = args[0];
 
-const readable = checkInput(filePath);
+// Check input values.
+const checkInputResult = checkInput(filePath);
 
-switch (readable) {
+// Process checkInput result (either an error or a readable stream).
+switch (checkInputResult) {
     case InputErrorEnum.MISSING_FILE_ARG:
         writeLine("One single argument representing a path to file is required.");
         process.exit(1);
@@ -30,16 +27,27 @@ switch (readable) {
         process.exit(3);
         break;
     default:
-        const configurationParser = new ConfigurationParser(readable);
-        configurationParser.parse(
-            (mower, mowerInstructions) => {
-                mowerInstructions.forEach(
-                    (instruction: InstructionEnum) => {
-                        if (mower) {
-                            mower.step(instruction);
-                        }
-                    },
-                );
-                writeLine(`${mower.x}${mower.y}${orientationEnumToLabel.get(mower.orientation)}`);
-            });
+        processFile(checkInputResult);
+}
+
+function processFile(readableStream: Readable) {
+    // Create a map that associates an OrientationEnum to the letter that shall be output.
+    const orientationEnumToLabel = new Map<OrientationEnum, string>();
+    orientationMapper.forEach(
+        function fillOrientationEnumToLabel(value, key) {
+            this.set(value, key);
+        }, orientationEnumToLabel);
+    const configurationParser = new ConfigurationParser(readableStream);
+    // Process the file line by line
+    configurationParser.parse(
+        (mower, mowerInstructions) => {
+            mowerInstructions.forEach(
+                (instruction: InstructionEnum) => {
+                    if (mower) {
+                        mower.step(instruction);
+                    }
+                },
+            );
+            writeLine(`${mower.x}${mower.y}${orientationEnumToLabel.get(mower.orientation)}`);
+        });
 }
